01b04356f98d88ffb70090bc384e5880bfe017cd
Amal A M <vectorhero007@gmail.com>
2025-06-24 17:57:52 +0530

    Listview

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -53,7 +53,7 @@ class _HomeViewState extends State<HomeView> {
                   // Top lvl Task info
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(AppStr.mainTitle, style: textTheme.displayLarge),
                       3.h,
@@ -67,11 +67,85 @@ class _HomeViewState extends State<HomeView> {
                 ],
               ),
             ),
-           
+
             // Divider
             Padding(
               padding: const EdgeInsets.only(top: 10.0),
-              child: Divider(thickness: 2, indent: 100),
+              child: Divider(thickness: 2, indent: 0),
+            ),
+
+            // Tasks
+            Expanded(
+              child: ListView.builder(
+                itemCount: 20,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  return AnimatedContainer(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(30, 69, 104, 220),
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(10, 0, 0, 0),
+                          offset: const Offset(0, 4),
+                          blurRadius: 10,
+                        ),
+                      ],
+                    ),
+                    duration: const Duration(milliseconds: 600),
+
+                    child: ListTile(
+                      // check icon
+                      leading: GestureDetector(
+                        onTap: () {
+                          // check or uncheck the task
+                        },
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 600),
+                          width: 30, // ✅ specify width
+                          height: 30, // ✅ specify height
+                          decoration: BoxDecoration(
+                            color: AppColors.primaryColor,
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.grey, width: .8),
+                          ),
+                          child: const Icon(Icons.check, color: Colors.white),
+                        ),
+                      ),
+
+                      // Task title
+                      title: Padding(
+                        padding: const EdgeInsets.only(bottom: 5, top: 3),
+                        child: const Text(
+                          "Done",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+
+                      // Task subtitle
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Descrption',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
