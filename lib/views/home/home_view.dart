e9c5d05d57be2824ffcf67fa7d13db0a8cbcfcd3
Amal A M <vectorhero007@gmail.com>
2025-06-24 12:10:10 +0530

    Custom app bar completed

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,7 +1,9 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:todo/extensions/space_exs.dart';
 import 'package:todo/utils/app_colors.dart';
+import 'package:todo/utils/app_str.dart';
 import 'package:todo/views/home/widgets/fab.dart';
 
 class HomeView extends StatefulWidget {
@@ -14,11 +16,56 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
       backgroundColor: Colors.white,
 
-      floatingActionButton: Fab(),
+      floatingActionButton: const Fab(),
+
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [
+            // Custom App Bar
+            Container(
+              margin: const EdgeInsets.only(top: 60),
+              width: double.infinity,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 30,
+                    height: 30,
+                    child: CircularProgressIndicator(
+                      value: 1 / 4,
+                      backgroundColor: Colors.grey,
+                      valueColor: AlwaysStoppedAnimation(
+                        AppColors.primaryColor,
+                      ),
+                    ),
+                  ),
+                  // space
+                  25.w,
+
+                  // Top lvl Task info
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(AppStr.mainTitle, style: textTheme.displayLarge),
+                      3.h,
+                      Text('1 of 3 task', style: textTheme.titleMedium),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
