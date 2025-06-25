3495e636ec1beb2efbf84d5e903bbf34c6ab903c
Amal A M <vectorhero007@gmail.com>
2025-06-25 11:11:18 +0530

    slider

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
 import 'package:lottie/lottie.dart';
 
 import 'package:todo/extensions/space_exs.dart';
@@ -29,115 +30,124 @@ class _HomeViewState extends State<HomeView> {
 
       floatingActionButton: const Fab(),
 
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            // Custom App Bar
-            Container(
-              margin: const EdgeInsets.only(top: 60),
-              width: double.infinity,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 30,
-                    height: 30,
-                    child: CircularProgressIndicator(
-                      value: 1 / 4,
-                      backgroundColor: Colors.grey,
-                      valueColor: AlwaysStoppedAnimation(
-                        AppColors.primaryColor,
-                      ),
+      body: SliderDrawer(
+        // Drawer
+        slider: Container(),
+
+        // Main Body
+        child: _buildHomeBody(textTheme),
+      ),
+    );
+  }
+
+  // Home body
+  Widget _buildHomeBody(TextTheme textTheme) {
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        children: [
+          // Custom App Bar
+          Container(
+            margin: const EdgeInsets.only(top: 60),
+            width: double.infinity,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 30,
+                  height: 30,
+                  child: CircularProgressIndicator(
+                    value: 1 / 4,
+                    backgroundColor: Colors.grey,
+                    valueColor: AlwaysStoppedAnimation(AppColors.primaryColor),
+                  ),
+                ),
+                // space
+                25.w,
+
+                // Top lvl Task info
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(AppStr.mainTitle, style: textTheme.displayLarge),
+                    3.h,
+                    Text(
+                      '1 of 3 task',
+                      style: textTheme.titleMedium,
+                      textAlign: TextAlign.left,
                     ),
-                  ),
-                  // space
-                  25.w,
-
-                  // Top lvl Task info
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(AppStr.mainTitle, style: textTheme.displayLarge),
-                      3.h,
-                      Text(
-                        '1 of 3 task',
-                        style: textTheme.titleMedium,
-                        textAlign: TextAlign.left,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              ],
             ),
+          ),
 
-            // Divider
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0),
-              child: Divider(thickness: 2, indent: 0),
-            ),
+          // Divider
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Divider(thickness: 2, indent: 0),
+          ),
 
-            // Tasks
-            Expanded(
-              child:
-                  testing.isNotEmpty
-                      // Task list is not empty
-                      ? ListView.builder(
-                        itemCount: testing.length,
-                        scrollDirection: Axis.vertical,
-                        itemBuilder: (context, index) {
-                          return Dismissible(
-                            direction: DismissDirection.horizontal,
-                            onDismissed: (_) {
-                              // We will remove current task from db
-                            },
-                            background: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Icon(
-                                  Icons.delete_outline,
-                                  color: Colors.grey,
-                                ),
-                                8.w,
-                                const Text(
-                                  AppStr.deleteTask,
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                              ],
-                            ),
-                            key: Key(index.toString()),
-                            child: const TaskWidget(),
-                          );
-                        },
-                      )
-                      // Task list is empty
-                      : Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          // Lottie Anime
-                          FadeIn(
-                            child: SizedBox(
-                              width: 200,
-                              height: 200,
-                              child: Lottie.asset(
-                                lottieURL,
-                                animate: testing.isNotEmpty ? false : true,
+          // Tasks
+          Expanded(
+            child:
+                testing.isNotEmpty
+                    // Task list is not empty
+                    ? ListView.builder(
+                      itemCount: testing.length,
+                      scrollDirection: Axis.vertical,
+                      itemBuilder: (context, index) {
+                        return Dismissible(
+                          direction: DismissDirection.horizontal,
+                          onDismissed: (_) {
+                            // We will remove current task from db
+                          },
+                          background: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(
+                                Icons.delete_outline,
+                                color: Colors.grey,
                               ),
+                              8.w,
+                              const Text(
+                                AppStr.deleteTask,
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                          key: Key(index.toString()),
+                          child: const TaskWidget(),
+                        );
+                      },
+                    )
+                    // Task list is empty
+                    : Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Lottie Anime
+                        FadeIn(
+                          child: SizedBox(
+                            width: 200,
+                            height: 200,
+                            child: Lottie.asset(
+                              lottieURL,
+                              animate: testing.isNotEmpty ? false : true,
                             ),
                           ),
+                        ),
 
-                          FadeInUp(
-                            from: 30,
-                            child: const Text(AppStr.doneAllTask),
-                          ),
-                        ],
-                      ),
-            ),
-          ],
-        ),
+                        FadeInUp(
+                          from: 30,
+                          child: const Text(AppStr.doneAllTask),
+                        ),
+                      ],
+                    ),
+          ),
+        ],
       ),
     );
   }
