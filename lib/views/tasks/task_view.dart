430d35f7ca542b08f9dacc17d54a0683391c00c2
Amal A M <vectorhero007@gmail.com>
2025-06-26 19:39:28 +0530

    TextField

--- lib/views/tasks/task_view.dart
+++ lib/views/tasks/task_view.dart
@@ -14,43 +14,87 @@ class _TaskViewState extends State<TaskView> {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
-    return Scaffold(
-      // AppBar
-      appBar: TaskViewApp(),
-      backgroundColor: Colors.white,
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
+      child: Scaffold(
+        // AppBar
+        appBar: TaskViewApp(),
+        backgroundColor: Colors.white,
 
-      // body
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(width: 70, child: Divider(thickness: 2)),
-                  RichText(
-                    text: TextSpan(
-                      text: AppStr.addNewTask,
-                      style: textTheme.titleLarge,
-                      children: [
-                        TextSpan(
-                          text: AppStr.taskStrnig,
-                          style: TextStyle(fontWeight: FontWeight.w500),
-                        ),
-                      ],
+        // body
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            children: [
+              // Top side texts
+              _buildTopSideTexts(textTheme),
+
+              SizedBox(
+                width: double.infinity,
+                height: 530,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Title of TextFiled
+                    Padding(
+                      padding: EdgeInsets.only(left: 30),
+                      child: Text(
+                        AppStr.titleOfTitleTextField,
+                        style: textTheme.headlineMedium,
+                      ),
                     ),
-                  ),
-                  SizedBox(width: 70, child: Divider(thickness: 2)),
-                ],
+
+                    Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.symmetric(horizontal: 16),
+                      child: TextFormField(
+                        maxLength: 6,
+                        cursorHeight: 24,
+                        style: TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey.shade300),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey.shade300),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildTopSideTexts(TextTheme textTheme) {
+    return SizedBox(
+      width: double.infinity,
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(width: 70, child: Divider(thickness: 2)),
+          RichText(
+            text: TextSpan(
+              text: AppStr.addNewTask,
+              style: textTheme.titleLarge,
+              children: [
+                TextSpan(
+                  text: AppStr.taskStrnig,
+                  style: TextStyle(fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 70, child: Divider(thickness: 2)),
+        ],
       ),
     );
   }
