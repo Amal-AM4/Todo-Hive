2ec1a99c30f21fc4f1a320bf8c4c42df87935433
Amal A M <vectorhero007@gmail.com>
2025-06-26 18:18:47 +0530

    Heading is completed in taskview page

--- lib/main.dart
+++ lib/main.dart
@@ -60,17 +60,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class TestPage extends StatefulWidget {
-//   const TestPage({super.key});
-
-//   @override
-//   State<TestPage> createState() => _TestPageState();
-// }
-
-// class _TestPageState extends State<TestPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(appBar: AppBar(backgroundColor: Colors.blue));
-//   }
-// }
--- lib/views/tasks/task_view.dart
+++ lib/views/tasks/task_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/utils/app_str.dart';
 import 'package:todo/views/tasks/widgets/task_view_app.dart';
 
 class TaskView extends StatefulWidget {
@@ -11,6 +12,46 @@ class TaskView extends StatefulWidget {
 class _TaskViewState extends State<TaskView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(appBar: TaskViewApp());
+    final textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      // AppBar
+      appBar: TaskViewApp(),
+      backgroundColor: Colors.white,
+
+      // body
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(width: 70, child: Divider(thickness: 2)),
+                  RichText(
+                    text: TextSpan(
+                      text: AppStr.addNewTask,
+                      style: textTheme.titleLarge,
+                      children: [
+                        TextSpan(
+                          text: AppStr.taskStrnig,
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 70, child: Divider(thickness: 2)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
