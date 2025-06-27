e29b61e6fbca720b3aa33a79d275f26cea683c54
Amal A M <vectorhero007@gmail.com>
2025-06-27 11:37:47 +0530

    code splite

--- lib/views/tasks/task_view.dart
+++ lib/views/tasks/task_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'package:todo/extensions/space_exs.dart';
 import 'package:todo/utils/app_str.dart';
+import 'package:todo/views/tasks/components/date_time_selection.dart';
 import 'package:todo/views/tasks/components/rep_textfield.dart';
 import 'package:todo/views/tasks/widgets/task_view_app.dart';
 
@@ -153,58 +154,6 @@ class _TaskViewState extends State<TaskView> {
           ),
           SizedBox(width: 70, child: Divider(thickness: 2)),
         ],
-      ),
-    );
-  }
-}
-
-class DateTimeSelectionWidget extends StatelessWidget {
-  const DateTimeSelectionWidget({
-    super.key,
-    required this.onTap,
-    required this.title,
-  });
-
-  final VoidCallback onTap;
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    var textTheme = Theme.of(context).textTheme;
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 16),
-        width: double.infinity,
-        height: 55,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.grey.shade400),
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text(title, style: textTheme.headlineSmall),
-            ),
-
-            Container(
-              margin: EdgeInsets.only(right: 10),
-              width: 80,
-              height: 35,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.grey.shade200,
-              ),
-              child: Center(
-                // This text will show Date Time as Time
-                child: Text(title, style: textTheme.titleSmall),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
